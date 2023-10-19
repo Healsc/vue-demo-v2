aab19e18b8c8449c4ac280b7d92cc4ba498cf919
sunchuang <sunchuang@maoyan.com>
2023-10-19 11:59:13 +0800

    feat:请求路由更改

--- vite.config.js
+++ vite.config.js
@@ -18,7 +18,7 @@ export default defineConfig({
     open: false,
     proxy: {
       "/api": {
-        target: "http://127.0.0.1:3000/test",
+        target: "http://127.0.0.1:3003/api",
         changeOrigin: true,
         rewrite: (path) => path.replace(/^\/api/, ""),
       },
