b9f611176f55620a0a9d4d963ad901f1271bf4cd
sunchuang <sunchuang@maoyan.com>
2023-10-27 17:18:20 +0800

    feat:侧边栏

---
+++ src/components/side-nav.js
@@ -0,0 +1,36 @@
+export const sideNavList = [
+  {
+    path: "/operation",
+    key: "key1",
+    label: "菜单1",
+    children: [
+      {
+        path: "/p1",
+        key: "key2",
+        label: "菜单1-1",
+      },
+      {
+        path: "/p2",
+        key: "key3",
+        label: "菜单1-2",
+      },
+    ],
+  },
+  {
+    path: "/operationa",
+    key: "key4",
+    label: "菜单2",
+    children: [
+      {
+        path: "/p3",
+        key: "key5",
+        label: "菜单2-1",
+      },
+    ],
+  },
+  {
+    path: "/p4",
+    key: "key6",
+    label: "菜单3",
+  },
+];
